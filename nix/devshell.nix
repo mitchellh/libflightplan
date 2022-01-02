b2d2df64d1a4dd2c3b9ff46f7a9dfefaa5603042
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-01-01 20:13:57 -0800

    update zig

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -1,9 +1,17 @@
 { mkShell
+
+, pkg-config
+, libxml2
 , zig
 }: mkShell rec {
   name = "libflightplan";
 
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
   buildInputs = [
+    libxml2
     zig
   ];
 }
