419603581c68ed5265f45fbee8d2ba61508a5576
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-01-08 14:38:29 -0800

    c example

--- examples/basic.c
+++ examples/basic.c
@@ -3,14 +3,17 @@
 #include <libflightplan.h>
 
 int main() {
+    // Parse our flight plan from an FPL file out of ForeFlight.
     flightplan *fpl = fpl_parse_garmin("./test/basic.fpl");
     if (fpl == NULL) {
         return 1;
     }
 
+    // Extract information from our flight plan easily
     printf("created at: %s\n\n", fpl_created(fpl));
-    printf("waypoints: %d\n", fpl_waypoints_count(fpl));
 
+    // Iterate through the available waypoints in the flightplan
+    printf("waypoints: %d\n", fpl_waypoints_count(fpl));
     flightplan_waypoint_iter *iter = fpl_waypoints_iter(fpl);
     while (1) {
         flightplan_waypoint *wp = fpl_waypoints_next(iter);
@@ -27,6 +30,7 @@ int main() {
     }
     fpl_waypoint_iter_free(iter);
 
+    // Iterate through the ordered route
     int max = fpl_route_points_count(fpl);
     printf("\nroute: \"%s\" (points: %d)\n", fpl_route_name(fpl), max);
     for (int i = 0; i < max; i++) {
