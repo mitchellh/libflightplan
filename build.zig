3353749c3500005ecfbec09189ef082173646db5
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2022-01-14 17:59:11 -0800

    Clean up build so it can cross-compile

--- build.zig
+++ build.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Builder = std.build.Builder;
+const libxml2 = @import("vendor/zig-libxml2/libxml2.zig");
 
 const ScdocStep = @import("src-build/ScdocStep.zig");
 
@@ -15,6 +16,7 @@ pub fn build(b: *Builder) !void {
     const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
 
+    // Options
     const man_pages = b.option(
         bool,
         "man-pages",
@@ -27,66 +29,79 @@ pub fn build(b: *Builder) !void {
         break :scdoc_found true;
     };
 
+    // Steps
+    const test_step = b.step("test", "Run all tests");
+    const test_unit_step = b.step("test-unit", "Run unit tests only");
+
+    // Build libxml2 for static builds only
+    const xml2 = try libxml2.create(b, target, mode, .{
+        .iconv = false,
+        .lzma = false,
+        .zlib = false,
+    });
+
+    // Native Zig tests
+    const lib_tests = b.addTestSource(pkgs.flightplan.path);
+    initNativeLibrary(lib_tests, mode, target);
+    xml2.link(lib_tests);
+    test_unit_step.dependOn(&lib_tests.step);
+    test_step.dependOn(&lib_tests.step);
+
     // Static C lib
-    const static_lib = b.addStaticLibrary("flightplan", "src/binding.zig");
-    initNativeLibrary(static_lib, mode, target);
-    static_lib.install();
+    {
+        const static_lib = b.addStaticLibrary("flightplan", "src/binding.zig");
+        initNativeLibrary(static_lib, mode, target);
+        xml2.addIncludeDirs(static_lib);
+        static_lib.install();
+        b.default_step.dependOn(&static_lib.step);
 
-    // Dynamic C lib
-    const dynamic_lib_name = if (target.isWindows())
-        "flightplan.dll"
-    else
-        "flightplan";
+        const static_binding_test = b.addExecutable("static-binding", null);
+        static_binding_test.setBuildMode(mode);
+        static_binding_test.setTarget(target);
+        static_binding_test.linkLibC();
+        static_binding_test.addIncludeDir("include");
+        static_binding_test.addCSourceFile("examples/basic.c", &[_][]const u8{ "-Wall", "-Wextra", "-pedantic", "-std=c99" });
+        static_binding_test.linkLibrary(static_lib);
+        xml2.link(static_binding_test);
 
-    const dynamic_lib = b.addSharedLibrary(dynamic_lib_name, "src/binding.zig", .unversioned);
-    initNativeLibrary(dynamic_lib, mode, target);
-    dynamic_lib.install();
+        const static_binding_test_run = static_binding_test.run();
 
+        test_step.dependOn(&static_binding_test_run.step);
+    }
+
+    // Dynamic C lib. We only build this if this is the native target so we
+    // can link to libxml2 on our native system.
+    if (target.isNative()) {
+        const dynamic_lib_name = if (target.isWindows())
+            "flightplan.dll"
+        else
+            "flightplan";
+
+        const dynamic_lib = b.addSharedLibrary(dynamic_lib_name, "src/binding.zig", .unversioned);
+        initNativeLibrary(dynamic_lib, mode, target);
+        dynamic_lib.linkSystemLibrary("libxml-2.0");
+        dynamic_lib.install();
+        b.default_step.dependOn(&dynamic_lib.step);
+
+        const dynamic_binding_test = b.addExecutable("dynamic-binding", null);
+        dynamic_binding_test.setBuildMode(mode);
+        dynamic_binding_test.setTarget(target);
+        dynamic_binding_test.linkLibC();
+        dynamic_binding_test.addIncludeDir("include");
+        dynamic_binding_test.addCSourceFile("examples/basic.c", &[_][]const u8{ "-Wall", "-Wextra", "-pedantic", "-std=c99" });
+        dynamic_binding_test.linkLibrary(dynamic_lib);
+
+        const dynamic_binding_test_run = dynamic_binding_test.run();
+        test_step.dependOn(&dynamic_binding_test_run.step);
+    }
+
+    // Headers
     const install_header = b.addInstallFileWithDir(
         .{ .path = "include/flightplan.h" },
         .header,
         "flightplan.h",
     );
     b.getInstallStep().dependOn(&install_header.step);
-
-    // Defaults when you do `zig build`
-    b.default_step.dependOn(&static_lib.step);
-    b.default_step.dependOn(&dynamic_lib.step);
-
-    // All tests
-    {
-        const lib_tests = b.addTestSource(pkgs.flightplan.path);
-        lib_tests.addIncludeDir("src/include");
-        lib_tests.addIncludeDir("include");
-        lib_tests.setBuildMode(mode);
-        lib_tests.linkLibC();
-        lib_tests.linkSystemLibrary("libxml-2.0");
-
-        const static_binding_test = b.addExecutable("static-binding", null);
-        static_binding_test.setBuildMode(mode);
-        static_binding_test.linkLibC();
-        static_binding_test.addIncludeDir("include");
-        static_binding_test.addCSourceFile("examples/basic.c", &[_][]const u8{ "-Wall", "-Wextra", "-pedantic", "-std=c99" });
-        static_binding_test.linkLibrary(static_lib);
-
-        const dynamic_binding_test = b.addExecutable("dynamic-binding", null);
-        dynamic_binding_test.setBuildMode(mode);
-        dynamic_binding_test.linkLibC();
-        dynamic_binding_test.addIncludeDir("include");
-        dynamic_binding_test.addCSourceFile("examples/basic.c", &[_][]const u8{ "-Wall", "-Wextra", "-pedantic", "-std=c99" });
-        dynamic_binding_test.linkLibrary(dynamic_lib);
-
-        const static_binding_test_run = static_binding_test.run();
-        const dynamic_binding_test_run = dynamic_binding_test.run();
-
-        const test_step = b.step("test", "Run all tests");
-        test_step.dependOn(&lib_tests.step);
-        test_step.dependOn(&static_binding_test_run.step);
-        test_step.dependOn(&dynamic_binding_test_run.step);
-
-        const test_unit_step = b.step("test-unit", "Run unit tests only");
-        test_unit_step.dependOn(&lib_tests.step);
-    }
 
     // pkg-config
     {
@@ -133,5 +148,4 @@ fn initNativeLibrary(
     lib.addIncludeDir("src/include");
     lib.addIncludeDir("include");
     lib.linkLibC();
-    lib.linkSystemLibrary("libxml-2.0");
 }
