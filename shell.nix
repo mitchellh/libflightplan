bd056680b0f1822fc52ed9a4a4b0360d0a4867f4
06kellyjac <dev@j-k.io>
2022-01-20 16:11:36 +0000

    pin flake-compat via the flake

--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
-(
-  import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
-    src = builtins.fetchGit ./.;
-  }
-).shellNix
+(import
+  (
+    let flake-compat = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.flake-compat; in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${flake-compat.locked.rev}.tar.gz";
+      sha256 = flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }).shellNix
