ca10c52e0d41f940d70d1c85f31ef7b3d2e3902e
06kellyjac <dev@j-k.io>
2022-01-20 16:11:36 +0000

    pin flake-compat via the flake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     zig.url = "github:arqv/zig-overlay";
+
+    flake-compat = { url = github:edolstra/flake-compat; flake = false; };
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
